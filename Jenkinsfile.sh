6d311dc1da9cd4b29bac886b011cab93200210e4
skinzer <sean.kinzer@gmail.com>
2017-05-04 15:11:56 -0700

    removed file

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -14,4 +14,4 @@ trap docker_cleanup EXIT
 docker_cleanup
 
 docker build -f Dockerfile-builder --no-cache -t  $d_image .
-docker run --env SRCCLR_API_TOKEN=testing --name $d_name $d_image
+docker run --env SRCCLR_API_TOKEN=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI0MSwiZXhwIjoxNTI0MjU1MzI1LCJhdXRob3JpdGllcyI6IkFHRU5UIiwianRpIjoiN2I1NzMxZmUtM2JkMi00ZTczLWFhMTYtNDQ1ZTNkNjZmZjljIiwiY2xpZW50X2lkIjoiIiwic2NvcGUiOltdfQ.uGydke-mt5aUwFdpgIgUNLKibfflESm9LzuNSSv5pXGrggXGDeVdd0yQPYTnIksIHTQIFVxAt941btTMR16DoJspAvg2BUdUihgXDAcBSTZRnUsRcNNRjQDJTOnJwmGAOJWsL-KmI7j4nEUXrQ03_KycWcddCWNz8fxcDVx11pI --name $d_name $d_image
